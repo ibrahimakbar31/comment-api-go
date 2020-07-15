2dee157ca6593ae441c4f8be4674b719374b2402
Ibrahim Akbar <ibrahim.akbar31@gmail.com>
2020-07-16 02:04:01 +0700

    remove test router

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/asaskevich/govalidator v0.0.0-20200428143746-21a406dcc535
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-playground/validator/v10 v10.2.0
 	github.com/hashicorp/go-version v1.2.1
 	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
 	github.com/jinzhu/gorm v1.9.14
--- router/index.go
+++ router/index.go
@@ -1,17 +1,12 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/ibrahimakbar31/comment-api-go/middleware"
 )
 
 //InitRouterV1 for initial route V1
 func InitRouterV1(routeGroup *gin.RouterGroup, app *middleware.App) {
-	routeGroup.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"status": "success"})
-	})
 	Auth(routeGroup, app)
 	Orgs(routeGroup, app)
 }
